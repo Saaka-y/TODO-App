88f678a69a286ce1187c0128895ba440ff3002e8
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-09-05 17:33:48 +0900

    Modified add function

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-const taskInput = document.getElementById('todo-input');
+const todoInput = document.getElementById('todo-input');
 const addButton = document.getElementById('add-btn');
-const todoList = document.getElementById('todo-list');
+const ul = document.getElementById('todo-list');
 const form = document.getElementById('form');
 
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-window.addEventListener('DOMContentLoaded', () => {
-  todos.forEach(task => {
-    createNewLi(task.name);
-  });
-});
+ul.innerHTML = ''; // 既存リストを一旦クリアしてから表示
 
+todos.forEach(todo => {
+  addTodo(todo);
+});  // ページ読み込み＆保存されているタスクを表示
 
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  addTask(); 
+  addTodo(); 
 });
 
 
-function createNewLi(taskName) {
-  const li = document.createElement('li');
-  li.className = 'todo-item';
 
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.className = 'todo-checkbox';
+function addTodo(todo) { // localStrageからの読み込み用に引数を追加
+  let todoText = todoInput.value.trim();
 
-  const span = document.createElement('span');
-  span.className = 'todo-name';
-  span.textContent = taskName;
+  if(todo && todo.text) {
+    todoText = todo.text;
+  }
 
-  li.appendChild(checkbox);
-  li.appendChild(span);
-  todoList.appendChild(li);
+  if (todoText.length > 0) {
+    const li = document.createElement('li');
+    li.classList.add("todo-item");
 
-  taskInput.value = '';
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.classList.add("todo-checkbox");
+
+    // localStrageからの読み込み時にchecked状態を復元
+    if(todo && todo.completed) {
+      checkbox.checked = true;
+    };
+
+    const span = document.createElement('span');
+    span.classList.add("todo-text");
+    span.textContent = todoText; 
+
+    checkbox.addEventListener('change', () => {
+      saveTodos();
+    });
+
+    // ✅ localStorageからの読み込みでない場合のみ保存する
+    if (!todo) {
+      saveTodos();
+    }
+
+    li.appendChild(checkbox)
+    li.appendChild(span);
+    ul.appendChild(li);
+    todoInput.value = '';
+  }
 }
 
+function saveTodos() {
+  const listItems = document.querySelectorAll('.todo-item');
+  const todos = [];
 
-function addTask() {
-  const task = taskInput.value.trim();
-  if (task === '') return;
-
-  todos.push({
-    name: task,
-    completed: false,
-  });
-
-  createNewLi(task, false);
+  listItems.forEach(item => {
+    let todo = {
+      text: item.querySelector('.todo-text').textContent,
+      completed: item.querySelector('.todo-checkbox').checked
+    }
+    todos.push(todo);
+  })
 
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 
-// checkedの状態を保存する処理が必要
-
-
-
-
-  
